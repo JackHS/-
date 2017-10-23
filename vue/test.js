a682c2f9f631ba2325f1ca2e864bbbb2481c379d
hu <learning129@163.com>
2017-10-23 23:38:54 +0800

    11111

--- vue/test.js
+++ vue/test.js
@@ -1,21 +1,5 @@
-var mychart = echarts.init(document.getElementById('pic'));
-var data = [{value:50,name:"IOS"},{value:500,name:"PC"}];
-var option = {
-    title: {
-        text: 'ECharts 入门示例'
-    },
-    tooltip: {},
-    legend: {
-        data:['销量']
-    },
-    xAxis: {
-        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-    },
-    yAxis: {},
-    series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
-    }]
-};
-mychart.setOption(option)
+$("document").ready(function(){
+    var c = "aaa"
+    var b = "input[data-id='"+c+"']"
+    console.log($(b).val())
+})
